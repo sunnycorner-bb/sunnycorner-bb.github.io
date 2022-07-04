48d538e069d2629a923c47c99c64a575d723d37e
蒼時弦也 <elct9620@frost.tw>
2022-07-05 01:17:02 +0800

    Debug PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
     let els = JSON.parse(content).htmlElements;
+
+    console.warn(els.tags.concat(els.classes, els.ids))
+    throw new Error(els.tags.concat(els.classes, els.ids))
     return els.tags.concat(els.classes, els.ids);
   }
 });
