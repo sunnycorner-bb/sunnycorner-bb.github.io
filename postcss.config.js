95e178fcefdda4c605a4414640f7b35b78c725b2
蒼時弦也 <elct9620@frost.tw>
2022-07-04 22:58:24 +0800

    Upgrade hugo to 0.101.0

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
     let els = JSON.parse(content).htmlElements;
-    throw new Error(els.tags.concat(els.classes, els.ids))
     return els.tags.concat(els.classes, els.ids);
   }
 });
