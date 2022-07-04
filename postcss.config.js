3beae8db938170fd125a2d4331cae87f498aeb33
蒼時弦也 <elct9620@frost.tw>
2022-07-05 01:55:17 +0800

    Manual run postcss in GitHub Actions

--- postcss.config.js
+++ postcss.config.js
@@ -2,16 +2,14 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
     let els = JSON.parse(content).htmlElements;
-    throw new Error('DEBUG FOR DISABLE POSTCSS')
     return els.tags.concat(els.classes, els.ids);
   }
 });
 
-
 module.exports = {
      plugins: [
          ...(
-           process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []
+           process.env.HUGO_ENVIRONMENT === 'production' ? [] : []
          )
      ]
  };
