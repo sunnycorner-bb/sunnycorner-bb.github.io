be55966a9b8b935962c8fe8abf82e55837329548
蒼時弦也 <elct9620@frost.tw>
2022-07-05 01:56:37 +0800

    Fix manual run postcss not executed

--- postcss.config.js
+++ postcss.config.js
@@ -8,8 +8,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
      plugins: [
-         ...(
-           process.env.HUGO_ENVIRONMENT === 'production' ? [] : []
-         )
+       purgecss
      ]
  };
