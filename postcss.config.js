57682f41b516cff4486fa6bcd5a8d80c69333b11
蒼時弦也 <elct9620@frost.tw>
2022-07-04 22:54:25 +0800

    Test purgecss work correct in GitHub Actions

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,16 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ './hugo_stats.json' ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
+  content: [ './hugo_stats.json' ],
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    throw new Error(els.tags.concat(els.classes, els.ids))
+    return els.tags.concat(els.classes, els.ids);
+  }
 });
 
 module.exports = {
      plugins: [
          ...(
-           process.env.HUGO_ENVIRONMENT === 'production' ? [] : []
+           process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []
          )
      ]
  };
