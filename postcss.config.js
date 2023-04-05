b551bd91b54f18ac9d8839c5844e642214b8922c
蒼時弦也 <elct9620@frost.tw>
2023-04-05 14:15:42 +0800

    Add safe list

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,20 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
     let els = JSON.parse(content).htmlElements;
-    return els.tags.concat(els.classes, els.ids);
-  }
+    return [
+      ...(els.tags || []),
+      ...(els.classes || []),
+      ...(els.ids || []),
+    ]
+  },
+  safelist: [
+    'input[type="checkbox"]+label:before, input[type="radio"]+label:before',
+    'input[type="checkbox"]+label:before'
+  ]
 });
 
 module.exports = {
      plugins: [
-       purgecss
+       ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
      ]
  };
